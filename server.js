8225d6523ea894187ce615c0496f6f29809ee5a9
Dustin heaps <dustinheaps89@gmail.com>
2019-06-03 18:40:28 -0400

    cors

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.post('/leaderboard', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, () => {
+
+app.listen(PORT, () => {
   console.log(`app is running on port ${PORT}`);
 });
