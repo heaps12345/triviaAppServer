a47ef04835d861b46ffc54890a33d2682526a596
Dustin heaps <dustinheaps89@gmail.com>
2019-06-03 17:59:07 -0400

    cors

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const knex = require('knex');
 
-
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'shakey45',
+
+    database: 'triviaLeaderboard'
   }
 });
 
@@ -16,6 +18,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+
+
 
 app.get('/leaderboard', (req, res) => {
   db.select('*')
