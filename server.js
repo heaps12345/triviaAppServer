5266e57b7d17d68b6783de1520aaf89b00b39eea
Dustin heaps <dustinheaps89@gmail.com>
2019-06-03 19:45:01 -0400

    2nd

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('It is working');
-});
-
 app.get('/leaderboard', (req, res) => {
   db.select('*')
     .from('users')
